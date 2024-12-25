5e394a75d04f06d19a903d733e195fd301a642f2
Jacob Fu <jjacobfu@gmail.com>
2024-12-25 07:32:11 -0500

    fixed desktop navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,52 @@ import { FaBars } from 'react-icons/fa';
 const Nav = () => {
 	const [menuOpen, setMenuOpen] = React.useState(false);
 	return (
-<>			<nav className='md-hidden'>
-				<button onClick={() => setMenuOpen(!menuOpen)} className='p-8 fixed top-0 right-0 z-50'>
+		<>
+			<nav className='hidden md:block md:w-1/2 fixed top-0 left-1/4 z-50'>
+				<div
+					className='flex justify-between mx-5 my-4 px-6 py-4 bg-[#06080C] rounded-2xl 
+      backdrop-blur-sm bg-opacity-60 items-center'>
+					<a href='#home' className='font-bold'>
+						hello! it's Jacob Fu
+					</a>
+					<div className='flex items-center gap-1'>
+						<a
+							href='#contact'
+							className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
+							Home
+						</a>
+						<a
+							href='#contact'
+							className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
+							Projects
+						</a>
+
+						<a
+							href='#contact'
+							className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
+							About
+						</a>
+						<a
+							href='#contact'
+							className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
+							Contact
+						</a>
+						<button
+							onClick={() => {
+								window.open('JacobFu_Resume.pdf', '_blank');
+							}}
+							className='bg-[#A534A0] text-white font-semibold px-5 py-2 rounded-xl hover:scale-105 transform transition duration-300 ease-in-out'>
+							Résumé
+						</button>
+					</div>
+				</div>
+			</nav>
+			
+
+
+
+			<nav className='sm:block md:hidden'>
+				<button onClick={() => setMenuOpen(!menuOpen)} className='md-hidden p-8 fixed top-0 right-0 z-50'>
 <div className='flex justify-center items-center bg-[#06080C] p-4 rounded-2xl     backdrop-blur-sm bg-opacity-60 active:scale-105 duration-300 ease-in-out'>
 						<FaBars size={30}/>
 	
@@ -15,18 +59,18 @@ const Nav = () => {
 	      backdrop-blur-sm bg-opacity-60 fixed right-8 p-4 top-24 z-50'>
 						<div className='flex flex-col gap-1'>
 							<a
-								href='#contact'
+								href='#home'
 								className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
 								Home
 							</a>
 							<a
-								href='#contact'
+								href='#projects'
 								className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
 								Projects
 							</a>
 	
 							<a
-								href='#contact'
+								href='#about'
 								className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
 								About
 							</a>
@@ -47,46 +91,6 @@ const Nav = () => {
 				) : (
 					''
 				)}
-			</nav>
-			<nav className='hidden sm:hidden md:w-1/2 fixed top-0 left-1/4 z-50'>
-
-			<div
-				className='flex justify-between mx-5 my-4 px-8 py-2 bg-[#06080C] rounded-3xl 
-      backdrop-blur-sm bg-opacity-60 items-center'>
-				<a href='#home' className='font-bold'>
-					hello! it's Jacob Fu
-				</a>
-				<div className='flex items-center gap-1'>
-					<a
-						href='#contact'
-						className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
-						Home
-					</a>
-					<a
-						href='#contact'
-						className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
-						Projects
-					</a>
-
-					<a
-						href='#contact'
-						className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
-						About
-					</a>
-					<a
-						href='#contact'
-						className='hover:bg-white hover:text-[#0D1117] hover:scale-105 transform transition duration-300 ease-in-out px-5 py-2 rounded-xl'>
-						Contact
-					</a>
-					<button
-						onClick={() => {
-							window.open('JacobFu_Resume.pdf', '_blank');
-						}}
-						className='bg-[#A534A0] text-white font-semibold px-5 py-2 rounded-xl hover:scale-105 transform transition duration-300 ease-in-out'>
-						Résumé
-					</button>
-				</div>
-			</div>
 			</nav>
 		</>
 	);
