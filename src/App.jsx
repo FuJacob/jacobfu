c079939fa4eb55cbdcbd043136075970aa4bce41
Jacob Fu <jjacobfu@gmail.com>
2024-12-25 02:33:54 -0500

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ import Timeline from './components/Timeline';
 import Contact from './components/Contact';
 import About from './components/About';
 import { BackgroundLines } from './components/background-lines';
+
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<Analytics />
 			<main className='p-12 min-h-screen bg-[#0D1117] text-white'>
 				<BackgroundLines className='flex items-center justify-center w-full flex-col px-4'>
 					<Nav />
